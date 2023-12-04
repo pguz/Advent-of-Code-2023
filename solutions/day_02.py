7c53b241977f04f224b86332b88a04c5dc337a48
Paweł Guz <pawelguz@gmail.com>
2023-12-04 07:46:08 +0100

    Day 02 - style

--- solutions/day_02.py
+++ solutions/day_02.py
@@ -1,5 +1,6 @@
 import re
 
+
 def parse_file(fd):
     re_game_data = re.compile(r"Game (?P<game_id>\d+): (?P<game_content>.*)")
     re_cubes = re.compile(r"(?P<cubes_number>\d+) (red|blue|green)")
@@ -8,11 +9,11 @@ def parse_file(fd):
         game = re_game_data.match(line)
         game_id, game_content = game['game_id'], game['game_content']
         games[game_id] = [
-            [
-                (int(cn), cc) for cn, cc in re_cubes.findall(cubes_subset)
-            ] for cubes_subset in game_content.split(';')
+            [(int(cn), cc) for cn, cc in re_cubes.findall(cubes_subset)]
+            for cubes_subset in game_content.split(';')
         ]
     return (games,)
+
 
 def task_1(games):
     cubes_max_number_validity = {
@@ -20,6 +21,7 @@ def task_1(games):
         'green': 13,
         'blue': 14,
     }
+
     def valid_game(game_content):
         for cubes_subset in game_content:
             for cubes_number, cube_color in cubes_subset:
@@ -27,7 +29,12 @@ def task_1(games):
                     return False
         return True
 
-    return sum(int(game_id) for game_id, game_content in games.items() if valid_game(game_content))
+    return sum(
+        int(game_id)
+        for game_id, game_content in games.items()
+        if valid_game(game_content)
+    )
+
 
 def task_2(games):
     def _calculate_power_set_cubes(cubes_set):
@@ -41,7 +48,9 @@ def task_2(games):
                 cubes[cube_color] = max(cubes_number, cubes[cube_color])
         return cubes['red'] * cubes['green'] * cubes['blue']
 
-    return sum(_calculate_power_set_cubes(game_content) for _, game_content in games.items())
+    return sum(
+        _calculate_power_set_cubes(game_content) for _, game_content in games.items()
+    )
 
 
 solution_function_01 = task_1
